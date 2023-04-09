21050b43dc3de544e3a9f1a8fc5f730d6bf5e182
twiggygnome <78651133+twiggygnome@users.noreply.github.com>
2023-04-09 16:48:20 +0200

    Build: Add basic structure to app

--- src/App.js
+++ src/App.js
@@ -4,7 +4,34 @@ import axios from "axios";
 function App() {
   const url = process.env.REACT_WEATHER_APP_API_KEY;
 
-  return <div></div>;
+  return (
+    <div className="app">
+      <div className="container">
+        <div className="top">
+          <div className="location">
+            <p>Malaga</p>
+          </div>
+          <div className="temp">
+            <h2>65°F</h2>
+          </div>
+          <div className="description">
+            <p>Clouds</p>
+          </div>
+        </div>
+        <div className="bottom">
+          <div className="feels">
+            <h2>70°F</h2>
+          </div>
+          <div className="humidity">
+            <p>20%</p>
+          </div>
+          <div className="wind">
+            <p>12 mph</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
