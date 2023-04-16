f4477d28b6538efbc0beb765d4cb883117289235
twiggygnome <78651133+twiggygnome@users.noreply.github.com>
2023-04-16 12:13:03 +0200

    Fix: Remove brackets from useState hook

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import axios from "axios";
 
 function App() {
   const [data, setData] = useState({});
-  const [location, setLocation] = useState({});
+  const [location, setLocation] = useState('');
 
   const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${process.env.REACT_APP_WEATHER_API_KEY}`
 
